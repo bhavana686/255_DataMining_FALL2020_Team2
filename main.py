aecbe622f20f36073d90edd5dd0cb996574dd161
Akhil Reddy <akhilreddymandadi@gmail.com>
2020-11-10 20:08:09 -0800

    Handling missing data and cleaning up data

--- main.py
+++ main.py
@@ -14,6 +14,37 @@ def remove_unnecessary_columns(data):
     data.drop(['name', 'streetaddress', 'day', 'latitude', 'longitude', 'geo_id'], axis=1, inplace=True)
 
 
+def fill_missing_values(data):
+    # Identifying the rows of age with non numeric values
+    age = pd.to_numeric(data.age, errors='coerce')
+    idx = age.isna()
+
+    # There are total of 4 rows with age as Unknown. Found it by printing rows with non numeric values
+    # Printing Non numeric rows
+    print(data[idx])
+
+    # Replacing Unknown values with zero
+    data["age"] = data["age"].replace("Unknown", '0')
+    data[["age"]] = data[["age"]].apply(pd.to_numeric)
+
+    # Plotted the number of distinct age values on x-axis and occurrences on y-axis and the graph was
+    # skewed distribution. So median is preferred to replace the missing values
+    plt.plot(data["age"].value_counts())
+    plt.show()
+
+    median_age = data['age'].median()
+    data["age"] = data["age"].replace(0, round(median_age))
+    print(data.dtypes)
+
+    print(data["gender"].value_counts())
+    print(data["raceethnicity"].value_counts())
+    data = data[data.raceethnicity != 'Unknown']
+    print(data["armed"].value_counts())
+    print(data["cause"].value_counts())
+    data = data[data.armed != 'Unknown']
+    data = data[data.cause != 'Unknown']
+    print(len(data))
+    return data
 
 def main():
     pd.set_option('display.width', 800)
@@ -27,6 +58,10 @@ def main():
     # as they are not necessary for the analysis
     # So dropping these columns will not affect the dataset
     remove_unnecessary_columns(data)
+    data = fill_missing_values(data)
+    # Printing the missing values
+    print(data.isnull().sum())
+
 
 
 if __name__ == "__main__":
